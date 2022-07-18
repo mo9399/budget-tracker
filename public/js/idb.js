798cd234a2f410877e68d891e6ab043f221c3235
your name <you@example.com>
2022-07-18 11:52:52 -0700

    add request for onsuccess

--- public/js/idb.js
+++ public/js/idb.js
@@ -5,3 +5,11 @@ request.onupgradeneeded = function (event) {
   const db = event.target.result;
   db.createObjectStore("new_transaction", { autoIncrement: true });
 };
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+  
+    if (navigator.online) {
+     loadTransaction();
+    }
+  };
